494bc7816acf1461f396fe6c0752ac6b9cf6dc7d
OmegaPhil <OmegaPhil@gmail.com>
2014-07-25 17:42:33 +0100

    Update copyright years

--- src/applet.cpp
+++ src/applet.cpp
@@ -1,7 +1,7 @@
 /* Implementation of the Applet class.
  *
  * Copyright (c) 2003, 04, 05 Ole Laursen.
- * Copyright (c) 2013 OmegaPhil (OmegaPhil+hardware.monitor@gmail.com)
+ * Copyright (c) 2013-2014 OmegaPhil (OmegaPhil+hardware.monitor@gmail.com)
  *
  * This program is free software; you can redistribute it and/or 
  * modify it under the terms of the GNU General Public License as 
